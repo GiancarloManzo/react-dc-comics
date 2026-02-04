daee0e39d149774eb39a8ec80dc5d0fd20df9b85
Giancarlo <cooper458.gm@gmail.com>
2026-02-04 16:30:40 +0100

    added Jumbotron component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Jumbotron from "./components/Jumbotron";
 
 export default function App() {
   return (
     <>
       <Header />
+      <Jumbotron />
       <h1>Dc Comics</h1>
     </>
   );

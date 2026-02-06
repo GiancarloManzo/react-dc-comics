f02656f56f4b7f1c2181173821dc1711d9401c7f
Giancarlo <cooper458.gm@gmail.com>
2026-02-06 15:48:32 +0100

    added bluebandlinks.js

---
+++ src/data/blueBandLinks.js
@@ -0,0 +1,29 @@
+const blueBandLinks = [
+  {
+    text: "DIGITAL COMICS",
+    icon: "/blue-band/buy-comics-digital-comics.png",
+    href: "#",
+  },
+  {
+    text: "DC MERCHANDISE",
+    icon: "/blue-band/buy-comics-merchandise.png",
+    href: "#",
+  },
+  {
+    text: "SUBSCRIPTION",
+    icon: "/blue-band/buy-comics-subscriptions.png",
+    href: "#",
+  },
+  {
+    text: "COMIC SHOP LOCATOR",
+    icon: "/blue-band/buy-comics-shop-locator.png",
+    href: "#",
+  },
+  {
+    text: "DC POWER VISA",
+    icon: "/blue-band/buy-dc-power-visa.svg",
+    href: "#",
+  },
+];
+
+export default blueBandLinks;

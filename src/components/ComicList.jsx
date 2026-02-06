74e157c68094265786370ce34d529b83cc53aa4b
Giancarlo <cooper458.gm@gmail.com>
2026-02-06 15:39:35 +0100

    added some bootstrap to classlist

--- src/components/ComicList.jsx
+++ src/components/ComicList.jsx
@@ -2,16 +2,26 @@ import ComicCard from "./ComicCard";
 
 export default function ComicList({ comics }) {
   return (
-    <section>
-      <h2>CURRENT SERIES</h2>
+    <section className="bg-dark text-white py-5">
+      <div className="container">
+        <h2 className="bg-primary d-inline-block px-3 py-2 mb-4">
+          CURRENT SERIES
+        </h2>
 
-      <div>
-        {comics.map((comic) => (
-          <ComicCard key={comic.id} comic={comic} />
-        ))}
+        <div className="row g-4">
+          {comics.map((comic) => (
+            <div className="col-6 col-md-4 col-lg-2" key={comic.id}>
+              <ComicCard comic={comic} />
+            </div>
+          ))}
+        </div>
+
+        <div className="text-center mt-4">
+          <button type="button" className="btn btn-primary px-4">
+            LOAD MORE
+          </button>
+        </div>
       </div>
-
-      <button type="button">Load More</button>
     </section>
   );
 }

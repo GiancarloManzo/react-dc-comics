391e02c1b00abbd3a15cdb7bc5a021bfc71fd5e9
Giancarlo <cooper458.gm@gmail.com>
2026-02-06 15:12:54 +0100

    added navLinks.js

---
+++ src/data/navLinks.js
@@ -0,0 +1,14 @@
+const navLinks = [
+  { text: "CHARACTERS", href: "#" },
+  { text: "COMICS", href: "#", active: true },
+  { text: "MOVIES", href: "#" },
+  { text: "TV", href: "#" },
+  { text: "GAMES", href: "#" },
+  { text: "COLLECTIBLES", href: "#" },
+  { text: "VIDEOS", href: "#" },
+  { text: "FANS", href: "#" },
+  { text: "NEWS", href: "#" },
+  { text: "SHOP", href: "#" },
+];
+
+export default navLinks;
